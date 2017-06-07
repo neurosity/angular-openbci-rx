0aecfa2301785447a586fd68ae48f23020ebe129
Alex Castillo <alex@castillo.io>
2017-06-06 21:26:21 -0700

    oops typo

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,11 @@
 
-const BrainOvservable = require('openbci-rx').Cyton;
+const BrainObservable = require('openbci-rx').Cyton;
 const io = require('socket.io')(4301);
 
 const simulate = process.argv[2] === '--simulate';
 const options = { verbose: true, simulate };
 
-const brainwaves$ = BrainOvservable(options)
+const brainwaves$ = BrainObservable(options)
     .toMicrovolts()
     .bufferCount(256)
     .groupByChannel()
