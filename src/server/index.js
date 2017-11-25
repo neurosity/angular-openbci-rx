926048033eade01b17ef338d9358b28648d403d0
Alex Castillo <alex@castillo.io>
2017-11-24 23:26:25 -0800

    now using openbci-observable and eeg-pipes

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,27 @@
 
-const BrainObservable = require('openbci-rx').Cyton;
+const { Cyton } = require('openbci-observable');
+const { voltsToMicrovolts, groupByChannel } = require('eeg-pipes');
+const { bufferCount } = require('rxjs/operators');
 const io = require('socket.io')(4301);
 
 const simulate = process.argv[2] === '--simulate';
 const options = { verbose: true, simulate };
 
-const brainwaves$ = BrainObservable(options)
-    .toMicrovolts()
-    .bufferCount(256)
-    .groupByChannel()
-    .subscribe(sendToBrowser);
+async function init () {
+    const brain = new Cyton(options);
+    await brain.connect();
+    await brain.start();
+
+    brain.stream.pipe(
+        voltsToMicrovolts(),
+        bufferCount(256),
+        groupByChannel()
+    ).subscribe(sendToBrowser);
+}
 
 function sendToBrowser (buffer) {
     console.log(buffer);
     io.emit('metric:eeg', buffer);
 }
+
+init();
