347079cf802fa8159521ad7e93b2f02199e6b7bb
Alex Castillo <alex@castillo.io>
2017-11-25 14:20:38 -0800

    server: better simulation check

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,7 @@ const { Cyton } = require('openbci-observable');
 const { voltsToMicrovolts, bufferTime } = require('eeg-pipes');
 const io = require('socket.io')(4301);
 
-const simulate = process.argv[2] === '--simulate';
-const options = { verbose: true, simulate };
+const options = { verbose: true, simulate: process.argv.includes('--simulate') };
 
 async function init () {
     const brain = new Cyton(options);
