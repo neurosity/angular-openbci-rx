5a9ab7491f4f1ffdb67ec3170ea6458d4ec4c89e
Alex Castillo <alex@castillo.io>
2017-11-25 02:08:17 -0800

    smoothie now using sample.timestamp

--- src/app/time-series/time-series.component.ts
+++ src/app/time-series/time-series.component.ts
@@ -2,12 +2,9 @@ import { Component, ElementRef } from '@angular/core';
 import { OnInit, OnDestroy } from '@angular/core';
 import { SmoothieChart, TimeSeries } from 'smoothie';
 import { ChartService } from '../shared/chart.service';
-import { Observable } from 'rxjs/Observable';
 import { fromEvent } from 'rxjs/observable/fromEvent';
-import { interval } from 'rxjs/observable/interval';
-import { switchMap } from 'rxjs/operators/switchMap';
+import { mergeMap } from 'rxjs/operators/mergeMap';
 import { tap } from 'rxjs/operators/tap';
-import { zip } from 'rxjs/operators/zip';
 import * as io from 'socket.io-client';
 
 const wsUrl = 'http://localhost:4301';
@@ -22,18 +19,17 @@ export class TimeSeriesComponent implements OnInit, OnDestroy {
 
   constructor(private view: ElementRef, private chartService: ChartService) {}
   
-  plotDelay = 1000;
+  plotDelay = 3000;
   channels = 8;
   amplitudes = [];
   socket = io(wsUrl);
-  options = this.chartService.getChartSmoothieDefaults({ millisPerPixel: 8 });
+  options = this.chartService.getChartSmoothieDefaults({ millisPerPixel: 3 });
   colors = this.chartService.getColors();
   canvases = Array(this.channels).fill(0).map(() => new SmoothieChart(this.options));
   lines = Array(this.channels).fill(0).map(() => new TimeSeries());
   stream$ = fromEvent(this.socket, wsEvent)
     .pipe(
-      switchMap(x => (x as any)),
-      zip(interval(4), sample => sample),
+      mergeMap(sample => (sample as any)),
       tap(sample => this.draw(sample))
     );
   
@@ -60,7 +56,7 @@ export class TimeSeriesComponent implements OnInit, OnDestroy {
 
   draw (sample) {
     sample.data.forEach((amplitude, index) => {
-      this.lines[index].append(new Date().getTime(), amplitude);
+      this.lines[index].append(sample.timestamp, amplitude);
       this.amplitudes[index] = amplitude.toFixed(2);
     });
   }
