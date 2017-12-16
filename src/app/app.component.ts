d069246c015dfe51aac9612952e8bdd2dc0a2239
Alex Castillo <alex@castillo.io>
2017-12-16 08:46:57 -0800

    pass stream from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import * as io from 'socket.io-client';
+
+const wsUrl = 'http://localhost:4301';
+const wsEvent = 'metric/eeg';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnDestroy {
+  title = 'Brainwaves!';
+  
+  socket = io(wsUrl);
+  stream$ = fromEvent(this.socket, wsEvent);
+
+  ngOnDestroy () {
+    this.socket.removeListener(wsEvent);
+  }
 }
--- src/app/time-series/time-series.component.ts
+++ src/app/time-series/time-series.component.ts
@@ -1,16 +1,11 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, Input, ElementRef } from '@angular/core';
 import { OnInit, OnDestroy } from '@angular/core';
 import { SmoothieChart, TimeSeries } from 'smoothie';
 import { ChartService } from '../shared/chart.service';
-import { fromEvent } from 'rxjs/observable/fromEvent';
 import { interval } from 'rxjs/observable/interval';
 import { mergeMap } from 'rxjs/operators/mergeMap';
 import { zip } from 'rxjs/operators/zip';
 import { tap } from 'rxjs/operators/tap';
-import * as io from 'socket.io-client';
-
-const wsUrl = 'http://localhost:4301';
-const wsEvent = 'metric/eeg';
 
 const channelsByBoard = {
   cyton: 8,
@@ -22,26 +17,23 @@ const channelsByBoard = {
   templateUrl: 'time-series.component.html',
   styleUrls: ['time-series.component.css'],
 })
-export class TimeSeriesComponent implements OnInit, OnDestroy {
+export class TimeSeriesComponent implements OnInit {
 
   constructor(private view: ElementRef, private chartService: ChartService) {}
   
+  @Input()
+  stream$;
+
   boardName = 'ganglion';
   channels = channelsByBoard[this.boardName];
   plotDelay = 1000;
   amplitudes = [];
-  socket = io(wsUrl);
+  amplitudes$;
+  
   colors = this.chartService.getColors();
   options = this.chartService.getChartSmoothieDefaults();
   canvases = Array(this.channels).fill(0).map(() => new SmoothieChart(this.options));
   lines = Array(this.channels).fill(0).map(() => new TimeSeries());
-
-  stream$ = fromEvent(this.socket, wsEvent);
-  amplitudes$ = this.stream$
-    .pipe(
-      mergeMap(samples => (samples as any)),
-      zip(interval(4), sample => sample)
-    );
   
   ngAfterViewInit () {
     const channels = this.view.nativeElement.querySelectorAll('canvas');
@@ -52,7 +44,13 @@ export class TimeSeriesComponent implements OnInit, OnDestroy {
 
   ngOnInit () {
     this.addTimeSeries();
-    this.amplitudes$.subscribe(sample => this.updateAmplitude(sample));
+
+    this.amplitudes$ = this.stream$
+      .pipe(
+        mergeMap(samples => (samples as any)),
+        zip(interval(4), sample => sample)
+      ).subscribe(sample => this.updateAmplitude(sample));
+      
     this.stream$.subscribe(buffer => {
       (buffer as any).forEach(sample => this.draw(sample));
     });
@@ -77,10 +75,6 @@ export class TimeSeriesComponent implements OnInit, OnDestroy {
     sample.data.forEach((amplitude, index) => {
       this.amplitudes[index] = amplitude.toFixed(2);
     });
-  }
-
-  ngOnDestroy () {
-    this.socket.removeListener(wsEvent);
   }
 
 }
